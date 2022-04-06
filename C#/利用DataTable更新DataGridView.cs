4ecc963b03507d96d4c48095595c489d693c1b28
Ontisme <ontisme@gmail.com>
2022-04-06 08:26:24 +0800

    Update 利用DataTable更新DataGridView.cs

--- C#/利用DataTable更新DataGridView.cs
+++ C#/利用DataTable更新DataGridView.cs
@@ -1,8 +1,9 @@
 public DataTable dt = new DataTable();
 
-
+//刷新不需要Clear DataGridView，重新初始化一次綁定即可
 public void InitDataTable()
 {
+    dt = new DataTable();
     dt.Columns.Add("Round", typeof(int));
     dt.Columns.Add("BetTime", typeof(string));
     dt.Columns.Add("BetType", typeof(Image));
