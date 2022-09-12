4d3eab7b4071c62bd9f2945e022a614a0ceb10f9
Ontisme <ontisme@gmail.com>
2022-09-12 18:44:42 +0800

    Create 檢查Gmail帳號狀態.py

---
+++ Python/檢查Gmail帳號狀態.py
@@ -0,0 +1,34 @@
+import threading
+from ipaddress import IPv4Address, IPv6Address
+# pip install py3-validate-email
+from validate_email import validate_email
+
+
+lists = ["iqXOnxIWsM@gmail.com", "lLRVCIiLVZ@gmail.com", "DNeRuwtQrH@gmail.com", "DtsMCLkFoK@gmail.com"]
+
+lists2 = ["2ad991q6a0@lelsm.ink", "ate4wxqsen@lelsm.ink", "x87e4xv7s8@kongkong66.top", "c28fynh5hp@hkwx.vip"]
+
+def check_email_valid(email: str):
+    is_valid = validate_email(
+        email_address=email,
+        check_format=True,
+        check_blacklist=True,
+        check_dns=True,
+        dns_timeout=10,
+        check_smtp=True,
+        smtp_timeout=10,
+        smtp_helo_host='my.host.name',
+        smtp_from_address='my@from.addr.ess',
+        smtp_skip_tls=False,
+        smtp_tls_context=None,
+        smtp_debug=False,
+        address_types=frozenset([IPv4Address, IPv6Address]))
+
+    print(f"{email}: {is_valid}")
+
+
+for i in lists:
+    threading.Thread(target=check_email_valid, args=(i,)).start()
+
+for i in lists2:
+    threading.Thread(target=check_email_valid, args=(i,)).start()
