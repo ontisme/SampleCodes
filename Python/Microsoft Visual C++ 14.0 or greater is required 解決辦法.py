1c59456cfcac114f66ea59098ccbc6dd21ee3009
Ontisme <ontisme@gmail.com>
2022-09-28 02:00:20 +0800

    Create Microsoft Visual C++ 14.0 or greater is required 解決辦法.py

---
+++ Python/Microsoft Visual C++ 14.0 or greater is required 解決辦法.py
@@ -0,0 +1,3 @@
+# 下載 Build Tool：https://aka.ms/vs/17/release/vs_BuildTools.exe
+# CMD 執行
+vs_buildtools.exe --norestart --passive --downloadThenInstall --includeRecommended --add Microsoft.VisualStudio.Workload.NativeDesktop --add Microsoft.VisualStudio.Workload.VCTools --add Microsoft.VisualStudio.Workload.MSBuildTools
