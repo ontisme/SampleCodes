36a6af7bafd40dbff1d5ac6e5294b1c04875e982
Ontisme <ontisme@gmail.com>
2023-01-13 16:55:21 +0800

    Update Microsoft Visual C++ 14.0 or greater is required 解決辦法.py

--- Python/Microsoft Visual C++ 14.0 or greater is required 解決辦法.py
+++ Python/Microsoft Visual C++ 14.0 or greater is required 解決辦法.py
@@ -1,3 +1,8 @@
 # 下載 Build Tool：https://aka.ms/vs/17/release/vs_BuildTools.exe
 # CMD 執行
 vs_buildtools.exe --norestart --passive --downloadThenInstall --includeRecommended --add Microsoft.VisualStudio.Workload.NativeDesktop --add Microsoft.VisualStudio.Workload.VCTools --add Microsoft.VisualStudio.Workload.MSBuildTools
+
+# PowerShell 腳本
+$client = new-object System.Net.WebClient
+$client.DownloadFile('https://aka.ms/vs/17/release/vs_BuildTools.exe', 'vs_BuildTools.exe')
+.\vs_buildtools.exe --norestart --passive --downloadThenInstall --includeRecommended --add Microsoft.VisualStudio.Workload.NativeDesktop --add Microsoft.VisualStudio.Workload.VCTools --add Microsoft.VisualStudio.Workload.MSBuildTools
